42e2c8eb83e872b69c72a11afd1a4622190182bf
Olawale Olayinka Joseph <57357459+olinks@users.noreply.github.com>
2023-03-08 19:32:01 +0100

    working on the api endpoints for the front end

--- backend/index.js
+++ backend/index.js
@@ -24,6 +24,12 @@ const db = mysql.createPool({
     database:"wikicat"
 });
 
+// import promisify and sleep function
+const { promisify } = require('util');
+// sleep function to pause 
+const sleep = promisify(setTimeout);
+
+// middle wares
 app.use(cors());
 app.use(express.json());
 app.use(bodyparser.urlencoded({extended: true}));
@@ -35,7 +41,7 @@ var addy = "0xb552cf92e761c8c71f8de52ed10b0df6dcfa24ff";
 
 
 app.get('/',(req, res) => {
-    res.send(fetchAddress());
+
 });
 
 app.post('/api/insertHolders', (req, res) => {
@@ -45,21 +51,19 @@ app.post('/api/insertHolders', (req, res) => {
     });
 });
 
-app.get('/api/insertHolderInfo/:address&:username', (req, res) => {
-    const address = req.params.address;
-    const username = req.params.username;
-    // const phone = req.params.phone;
-    // const data = {address, username, phone};
+app.get('/api/insertHolderInfo/', (req, res) => {
+    const address = req.body.address;
+    const username = req.body.username;
+    const phone = req.body.phone;
 
-    res.send(username);
-    // const sql ="UPDATE holders SET username = ";
-    // db.query(sql, (err, result) => {
-    //     err ? res.send(err) : result ? res.send(result) : res.send('No result');
-    // });
+    const sql ="INSERT INTO holders (address, balance, username, phone) VALUES (?,?,?,?)";
+    db.query(sql, (err, result) => {
+        err ? res.send(err) : result ? res.send(result) : res.send('No result');
+    });
 });
 
 app.get('/api/updateHolderBalance/:balance&:address', (req, res) => {
-    const address = req.body.address;
+    const address = req.params.address;
     const newbal = req.params.balance;
     const sql ="UPDATE holders SET balance=? WHERE address=?";
     db.query(sql,[newbal,address], (err, result) => {
@@ -79,7 +83,7 @@ app.get('/getHolders',(req, res) => {
 })
 
 app.get('/api/getAllHolders',(req, res) => {
-    const sql = "SELECT * FROM holders WHERE balance > 0 ORDER BY balance DESC LIMIT 3";
+    const sql = "SELECT * FROM holders WHERE balance > 0 ORDER BY balance DESC LIMIT 10";
     // const sql = "SELECT * FROM holders  WHERE balance > 0";
     db.query(sql,(err, result) => {
         err ? res.send(err) : result ? res.send(result) : res.send('No result');
@@ -89,89 +93,29 @@ app.get('/api/getAllHolders',(req, res) => {
 
 app.get('/api/getWikicatData', (req, res) => {
 
-})
+});
+
+// wikicat total supply
+app.get('/api/getTokenSupply',(req,res) => {
+    axios.get(`https://api.bscscan.com/api?module=stats&action=tokensupply&contractaddress=${contractaddress}&apikey=${apikey}`)
+    .then((result) => {
+        res.send(result.data.result);
+    });
+});
+
+app.get('/api/getTokenCirculatingSupply', (req,res) => {
+    var circulatingSupply
+    axios.get(`https://api.bscscan.com/api?module=stats&action=tokenCsupply&contractaddress=${contractaddress}}&apikey=${apikey}`)
+    .then((result) => {
+        res.send(result.data.result);
+        console.log(result.data);
+    })
+});
 
 app.get('/api/getDtgData', (req, res) => {
 
-})
+});
 
-// axios.get("http://localhost:3001/getAllHolders")
-// .then(res =>{
-//     const address = res.data;
-//     console.log(address);
-//     var i = 1;
-//     address.forEach( a => {
-
-//             const getBal = async () => {
-//                  const me = axios.get(`https://api.bscscan.com/api?module=account&action=tokenbalance&contractaddress=${contractaddress}&address=${a.address}&tag=latest&apikey=${apikey}`);
-//                 const response = await me.then(res => {
-//                     console.log(res.data.result);
-//                 });
-//             } 
-
-//         console.log(i);
-//         console.log(a.address);
-//         getBal();
-
-//          i++;
-//     })
-// }
-// )
-
-// const sleep = (milliseconds) => {  
-//       return new Promise(resolve => setTimeout(resolve, milliseconds)); 
-// }
-
-axios.get("http://localhost:3001/api/getAllHolders").then((res) => {
-  const dbResult = res.data;
-
-  setInterval(() => {
-    var i = 1;
-    //   console.log(i,dbResult);
-
-      i++;
-  }, 20000);
-
-    dbResult.map(m => {
-        // console.log(m.address);
-    });
-
-//   console.log("dbresult =>",dbResult);
-  const mydb = dbResult.map( async (data) => {
-    const { address } = data;
-    // await sleep(1500);
-    setInterval(async () => {
-        const res = await 
-        axios.post(
-          `https://api.bscscan.com/api?module=account&action=tokenbalance&contractaddress=${contractaddress}&address=${address}&tag=latest&apikey=${apikey}`
-        ).then((res) => {
-            console.log("result", res);
-        }
-        );
-
-
-        /*
-        const balance = res.data.result;
-        const intodb = {address, balance};
-    
-        try{
-            // update Db
-            const query = "UPDATE holders SET balance=? WHERE address=?"
-            db.query(query,[intodb.balance, intodb.address],(err, result) => {
-                err ? console.log(err) : result ? console.log(intodb,result) : console.log("void");
-            })
-        }catch (e){
-            console.log("olayinka eeee eee eee eee eee eee eee eee eee eee eee eee eee ee error", e);
-        }
-        */
-
-
-        // console.log("some text",intodb);
-        // return intodb;
-    }, 20000);
-    })
-
-})
 // starting server and listening port
 app.listen(3001,()=>{
     console.log("server running, port 3001");
